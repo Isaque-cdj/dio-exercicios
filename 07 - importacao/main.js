20edbd70acd33ddca5db201df8766b5a2c4007c1
Isaque Coelho <isaquecoelho.dj@gmail.com>
2024-02-12 15:12:06 -0300

    Exercício Guiado - Maior Número Sorteado

--- 07 - importacao/main.js
+++ 07 - importacao/main.js
@@ -1,4 +1,20 @@
 
-const funcoes = require('./funcoes-auxiliares.js');
+const  {gets, print} = require('./funcoes-auxiliares.js');
 
-console.log(funcoes.gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++){
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++){
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor=numeroSorteado;
+    }
+}
+
+print(maiorValor);
